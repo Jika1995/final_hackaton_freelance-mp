d9bbe2b2714851a692bce97021b0d12329827835
Zamir <eazzy_freez@mail.ru>
2023-03-14 00:04:32 +0600

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import MainRoutes from "./MainRoutes";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Navbar />
+      <MainRoutes />
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/HomePage.css";
+
+const HomePage = () => {
+  return (
+    <div className="main">
+      <div className="d1"></div>
+      <div className="d2"></div>
+      <div className="d3"></div>
+      <div className="d4"></div>
+    </div>
+  );
+};
+
+export default HomePage;
