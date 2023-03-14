d738fc522d7be79aeb6944f9d457564ac437646e
Your Name <lajika@MacBook-Air-Jibek.local>
2023-03-14 12:56:36 +0600

    signup css

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import { Typography } from "@mui/material";
-import '../styles/RegistrationPage.css'
-
+import TextField from '@mui/material/TextField';
+import {Button} from "@mui/material";
+import '../styles/RegistrationPage.css';
 
 const RegistrationPage = () => {
   return <div className="auth-main">
@@ -11,9 +12,9 @@ const RegistrationPage = () => {
       <Box className='left-block'>
         <Box className='welcome-block'>
           <Box className='welcome-info'>
-            <img src="" alt="error" />
-            <Typography variant="h4">
-              Welcome to Free Hub!
+            <img src='https://i.ibb.co/CWxG1Th/Rectangle.png' width='150px' id="zamok"/>
+            <Typography variant="h4" style={{fontFamily: 'Mulish', fontWeight: '700', fontSize: '50px'}}>
+              WELCOME TO FREE HUB!
             </Typography>
             <p>
               Free Hub is the leading marketplace of freelance services. Made for freelancers all over the world building the future of digital world. 
@@ -21,12 +22,31 @@ const RegistrationPage = () => {
           </Box>
         </Box>
         <Box className='auth-block'>
-          <h1>this is auth block</h1>
+          <Box className='reg-block'>
+          <div className="sign-block">
+            <Typography variant="h2"  color='grey' style={{cursor: 'pointer', fontFamily: 'Mulish', fontWeight: '700', fontSize: '50px'}}>Sign in</Typography>
+            <Typography variant="h2"  color='rgba(0, 0, 0, 0.87)' style={{cursor: 'pointer', fontFamily: 'Mulish', fontWeight: '700', fontSize: '50px'}}>Sign up</Typography>
+          </div>
+          <div className="inputs-block">
+            <TextField variant='standard' label='Email*' placeholder='Enter your email' color='secondary' fullWidth sx={{mb: '10px'}} className='reg-inp'>
+              
+            </TextField>
+            <TextField variant='standard' label='Password*' placeholder='Enter your password' color='secondary'  fullWidth sx={{mb: '10px'}} className='reg-inp'>
+              
+            </TextField>
+            <TextField variant='standard' label='Password Confirmation*' placeholder='Confirm your password' color='secondary'  fullWidth sx={{mb: '10px'}} className='reg-inp'>
+              
+            </TextField>
+          </div>
+          <Button variant="contained" className="reg-btn" fullWidth>
+            Sign up
+          </Button>
+          </Box>
         </Box>
        
       </Box>
       <Box className='right-block'>
-        <img src="" alt="error" />
+        <img src="https://i.ibb.co/QC5Qfvn/3d-business-young-man-got-an-idea.png" alt="error" width='400px'/>
       </Box>
     </Container>
   </div>;
