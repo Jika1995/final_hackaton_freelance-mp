1937f6c73207c46c8b64d9b8bc7a14ed36c34312
Your Name <lajika@MacBook-Air-Jibek.local>
2023-03-14 13:28:10 +0600

    login css

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -5,8 +5,12 @@ import { Typography } from "@mui/material";
 import TextField from '@mui/material/TextField';
 import {Button} from "@mui/material";
 import '../styles/RegistrationPage.css';
+import { useNavigate } from 'react-router-dom';
 
 const RegistrationPage = () => {
+
+  const navigate = useNavigate();
+
   return <div className="auth-main">
     <Container className="auth-container">
       <Box className='left-block'>
@@ -24,8 +28,8 @@ const RegistrationPage = () => {
         <Box className='auth-block'>
           <Box className='reg-block'>
           <div className="sign-block">
-            <Typography variant="h2"  color='grey' style={{cursor: 'pointer', fontFamily: 'Mulish', fontWeight: '700', fontSize: '50px'}}>Sign in</Typography>
-            <Typography variant="h2"  color='rgba(0, 0, 0, 0.87)' style={{cursor: 'pointer', fontFamily: 'Mulish', fontWeight: '700', fontSize: '50px'}}>Sign up</Typography>
+            <Typography variant="h2"  color='grey' style={{cursor: 'pointer', fontFamily: 'Mulish', fontWeight: '700', fontSize: '50px'}} onClick={() => navigate('/login')} id='sign-nav'>Sign in</Typography>
+            <Typography variant="h2"  color='rgba(0, 0, 0, 0.87)' style={{cursor: 'pointer', fontFamily: 'Mulish', fontWeight: '700', fontSize: '50px'}} >Sign up</Typography>
           </div>
           <div className="inputs-block">
             <TextField variant='standard' label='Email*' placeholder='Enter your email' color='secondary' fullWidth sx={{mb: '10px'}} className='reg-inp'>
@@ -46,10 +50,12 @@ const RegistrationPage = () => {
        
       </Box>
       <Box className='right-block'>
-        <img src="https://i.ibb.co/QC5Qfvn/3d-business-young-man-got-an-idea.png" alt="error" width='400px'/>
+        <img src="https://i.ibb.co/xH1gDVL/kid-30637-unscreen.gif" alt="error" width='800px' height='500px' id='auth-img'/>
       </Box>
     </Container>
   </div>;
 };
 
 export default RegistrationPage;
+// https://i.ibb.co/k4hpvv5/3d-business-young-man-got-an-idea-removebg-preview.png
+// https://i.ibb.co/QC5Qfvn/3d-business-young-man-got-an-idea.png
