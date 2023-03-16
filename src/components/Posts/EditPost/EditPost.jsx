a3a0bdb6858a272b3f96aadb9dec6c256b706167
Zamir <eazzy_freez@mail.ru>
2023-03-16 20:17:13 +0600

    fix

--- src/components/Posts/EditPost/EditPost.jsx
+++ src/components/Posts/EditPost/EditPost.jsx
@@ -1,67 +1,91 @@
 import React, { useEffect, useState, useContext } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
-import {postsContext} from '../../../contexts/PostContextProvider';
+import { postsContext } from "../../../contexts/PostContextProvider";
 
 const EditPost = () => {
-    const {getOnePost, onePost, saveEditedPost} = useContext(postsContext);
-    const [post, setPost] = useState(onePost);
-    const navigate = useNavigate();
-    const { id } = useParams();
+  const { getOnePost, onePost, saveEditedPost } = useContext(postsContext);
+  const [post, setPost] = useState(onePost);
+  const navigate = useNavigate();
+  const { id } = useParams();
 
-    useEffect(() => {
-        getOnePost(id)
-      }, []);
-    
-      useEffect(() => {
-        setPost(onePost)
-      }, [onePost, ]);
+  useEffect(() => {
+    getOnePost(id);
+  }, []);
 
-      const handleInp = (e) => {
-        
-        if(e.target.name === 'image') {
-          let obj = {
-            ...post,
-            image: e.target.files[0]
-          }
-          setPost(obj);
-        } else {
-          let obj = {
-            ...post,
-            [e.target.name]: e.target.value
-          };
-          setPost(obj)
-        }
+  useEffect(() => {
+    setPost(onePost);
+  }, [onePost]);
 
-        // let newPost = new FormData();
-        // for (let i in post) {
-        //   newPost.append(`${i}`, post[i])
-        //   // console.log(post[i]);
-        // };
-        // console.log(newPost);
-        // setPost(newPost)
+  const handleInp = (e) => {
+    if (e.target.name === "image") {
+      let obj = {
+        ...post,
+        image: e.target.files[0],
+      };
+      setPost(obj);
+    } else {
+      let obj = {
+        ...post,
+        [e.target.name]: e.target.value,
+      };
+      setPost(obj);
+    }
 
-      }
-
-
+    // let newPost = new FormData();
+    // for (let i in post) {
+    //   newPost.append(`${i}`, post[i])
+    //   // console.log(post[i]);
+    // };
+    // console.log(newPost);
+    // setPost(newPost)
+  };
 
   return (
     <>
-    {post ? (<>
-    <h2>Edit Product</h2>
-    <input type="text" name="title" onChange={handleInp} value={post.title}/>
-    <input type="text" name="description" onChange={handleInp} value={post.description}/>
-    <input type="text" name="image" onChange={handleInp} value={post.image}/>
-    <input type="file" accept="image/*" name="image" onChange={handleInp} />
+      {post ? (
+        <>
+          <h2>Edit Product</h2>
+          <input
+            type="text"
+            name="title"
+            onChange={handleInp}
+            value={post.title}
+          />
+          <input
+            type="text"
+            name="description"
+            onChange={handleInp}
+            value={post.description}
+          />
+          <input
+            type="text"
+            name="image"
+            onChange={handleInp}
+            value={post.image}
+          />
+          <input
+            type="file"
+            accept="image/*"
+            name="image"
+            onChange={handleInp}
+          />
 
-    {/* <input type="text" name="price" onChange={handleInp} value={post.price}/> */}
-    <button onClick={() => {
-            saveEditedPost(post);
-            navigate('/posts')
-    }}>Save changes</button>
-    </>) : (<></>) }
+          {/* <input type="text" name="price" onChange={handleInp} value={post.price}/> */}
+          <button
+            onClick={() => {
+              saveEditedPost(post);
+              navigate("/posts");
+            }}
+          >
+            Save changes
+          </button>
+        </>
+      ) : (
+        <></>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default EditPost
+export default EditPost;
