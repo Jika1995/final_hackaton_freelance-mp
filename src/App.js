1c8b68e1174bf253b3d78fccb641d33facae26f6
Zamir <eazzy_freez@mail.ru>
2023-03-17 00:39:05 +0600

    add logic profile

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import Footer from "./components/Footer/Footer";
 import MainRoutes from "./MainRoutes";
 import AuthContextProvider from "./contexts/AuthContextProvider";
 import PostContextProvider from "./contexts/PostContextProvider";
+import ProfileContextProvider from "./contexts/ProfileContextProvider";
 
 const App = () => {
   return (
-    <PostContextProvider>
-    <AuthContextProvider>
-      <Navbar />
-      <MainRoutes />
-      <Footer />
-    </AuthContextProvider>
-    </PostContextProvider>
+    <ProfileContextProvider>
+      <PostContextProvider>
+        <AuthContextProvider>
+          <Navbar />
+          <MainRoutes />
+          <Footer />
+        </AuthContextProvider>
+      </PostContextProvider>
+    </ProfileContextProvider>
   );
 };
 
