dcc5a2008666a09b62f1de1a044a9da928099ca4
Your Name <lajika@MacBook-Air-Jibek.local>
2023-03-17 11:29:40 +0600

    add some css

--- src/components/Posts/PostCard/PostCard.jsx
+++ src/components/Posts/PostCard/PostCard.jsx
@@ -2,6 +2,14 @@ import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { authContext } from "../../../contexts/AuthContextProvider";
 import {postsContext} from '../../../contexts/PostContextProvider';
+import '../../../styles/PostCard.css';
+
+import { Button, Typography, Icon } from "@mui/material";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 const PostCard = ({item}) => {
   const navigate = useNavigate();
@@ -11,27 +19,46 @@ const PostCard = ({item}) => {
 
   return (
     <div className="main-postCard">
-    <h2>Title: {item.title}</h2>
-    <h2>by {item.owner}</h2>
-    <img
+      <div className="card-container">
+      <img
+      className="card-img"
       src={item.image}
       alt=""
-      style={{ width: "500px", height: "500px" }}
     />
-    <p>
-      <br />
-      {/* Likes: {item.likes} */}
-    </p>
-    <p>{item.description}</p>
-    {/* <button onClick={() => toggleLike(item.id)}>Like</button> */}
-    {item.owner === currentUser? (
-      <>
-        <button onClick={() => navigate(`/edit/${item.id}`)}>Edit</button>
-        <button onClick={() => deletePost(item.id)}>Delete</button>
-      </>
+    <div className="info-card">
+      <div style={{display: 'flex', justifyContent: "space-between", alignItems: 'center'}}>
+      <div className="card-titles">
+        <Typography variant="h4">{item.title}</Typography>
+        <p>by {item.owner}</p>
+      </div>
+      <div className="card-price">
+        <p>{item.price} $</p>
+      </div>
+      </div>
+      <div className="use-block">
+      {item.owner === currentUser? (
+      <div className="owner-btns">
+        <Button onClick={() => navigate(`/edit/${item.id}`)} className='edit-btn'>
+          Edit
+        </Button>
+        <Button onClick={() => deletePost(item.id)} className='delete-btn'>
+          Delete
+        </Button>
+      </div>
     ) : null}
+      <div>
+        <Button>View</Button>
+        <FavoriteIcon/>
+        <ShoppingCartIcon/>
+        <Button className="like-btn"><h2>100 {item.likes} </h2><ThumbUpIcon/> </Button>
+      </div>
+      </div>
+    </div>
+      </div>
   </div>
   )
 }
 
 export default PostCard
+
+// onClick={() => toggleLike(item.id)}
